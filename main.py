fffa04a3c74e5d8b97ac65e3fe3ea52b51de768f
Your Name <your-email@gmail.com>
2026-04-24 13:28:13 +0530

    Added multiple code files

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-n = int(input('Enter number of terms: '))
-a, b = 0, 1
-
-print('Fibonacci Series:')
-for i in range(n):
-    print(a, end=' ')
-    a, b = b, a + b
+print("Welcome to fibonacci-series")
+print("This is the main program file.")
